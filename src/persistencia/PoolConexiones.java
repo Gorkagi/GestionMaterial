0ca29a405fe1107610f3690b2ba0d150f5183b0c
Infamous-devel <infamous.devel@gmail.com>
2016-04-21 18:09:02 +0200

    Cambio de Contraseña DB

--- src/persistencia/PoolConexiones.java
+++ src/persistencia/PoolConexiones.java
@@ -41,7 +41,7 @@ class PoolConexiones
 	  if (instance == null){
 	    Properties connectionProps = new Properties();
 	    connectionProps.put("user", "root");
-	    //connectionProps.put("password", "root");
+	    connectionProps.put("password", "123");
 
 	   instance = DriverManager.getConnection(SGBDURL, connectionProps);
 	  }
